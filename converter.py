a4389eb0c522f2791fd415bd055056483e850c58
byarbrough <6315292+byarbrough@users.noreply.github.com>
2023-08-23 09:23:11 -0600

    Deliberate messy format
    Black and flake8 will clean this up

--- converter.py
+++ converter.py
@@ -1,30 +1,14 @@
-one_digit_words = {
-    "0": ["zero"],
-    "1": ["one"],
-    "2": ["two", "twen"],
-    3: ["three", "thir"],
-    "4": ["four", "for"],
-    "5": ["five", "fif"],
-    "6": ["six"],
-    "7": ["seven"],
-    "8": ["eight"],
-    "9": "nine",
-}
+'''
+Demo program for how black, flake8, and mypy can help you
+dev operable Python!
+'''
+import os
+
 
 two_digit_words = ["ten", "eleven", "twelve"]
 hundred = "hundred"
-large_sum_words = [
-    "thousand",
-    "million",
-    "billion",
-    "trillion",
-    "quadrillion",
-    "quintillion",
-    "sextillion",
-    "septillion",
-    "octillion",
-    "nonillion",
-]
+large_sum_words = ["thousand","million","billion","trillion","quadrillion","quintillion","sextillion","septillion","octillion","nonillion",]
+one_digit_words = {"0": ["zero"],"1": ["one"],"2": ["two", "twen"],3: ["three", "thir"],"4": ["four", "for"], "5": ["five", "fif"], "6": ["six"], "7": ["seven"],"8": ["eight"], "9": "nine", }
 
 
 def convert(n):
